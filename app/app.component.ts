caed2f4ea1a70deb1731e5ec9234d0bf04a55915
Steffen Tröster <steffen.troester@googlemail.com>
2016-01-31 20:43:06 +0100

    initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+import {Component} from 'angular2/core';
+import {Node} from './node/node.component';
+
+@Component({
+    selector: 'open100',
+    templateUrl: 'app/app.template.html',
+    directives: [Node]
+})
+export class AppComponent { }
---
+++ app/node/node.component.ts
@@ -0,0 +1,9 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'node',
+    templateUrl: 'app/node/node.template.html'
+})
+export class Node {
+
+}
